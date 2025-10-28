e7cb608ec0f5475b7af43dfda94de7de40f15c96
StephenLiu_WSL <stephen.liu21@outlook.com>
2025-10-28 21:14:22 +0000

    Add method for getting player winnings from pot, including cases of side pot

--- include/PokerEngine/core/pot.hpp
+++ include/PokerEngine/core/pot.hpp
@@ -15,14 +15,13 @@ public:
     void addContribution(PlayerId id, int chips);
     int getContribution(PlayerId id);
     int getTotal() const;
+    int getWinningsForPlayer(PlayerId id);
 
-
+    bool empty() { return contributions_.empty() || getTotal() == 0; }
     void clear() { contributions_.clear(); }
 
 private:
-    std::unordered_map<int, int> contributions_;
-
-
+    std::unordered_map<PlayerId, int> contributions_;
 };
 
 void Pot::addContribution(PlayerId id, int chips) {
@@ -41,6 +40,25 @@ int Pot::getTotal() const {
     return std::accumulate(contributions_.begin(),contributions_.end(),0,
         [](int sum, const auto& it) { return sum += it.second; }
     );
+}
+
+/**
+ * @brief Given player id, their winnings will be returned. They can only win as much money from other players as they have put into the pot.
+ * 
+ */
+int Pot::getWinningsForPlayer(PlayerId id) {
+    int player_max_winning_per_player = contributions_[id];
+    contributions_[id] = 0;
+    int total = player_max_winning_per_player;
+    for(auto [pid, chips] : contributions_) {
+        if(pid != id) {
+            int winnings_from_pid = chips < player_max_winning_per_player ? chips : player_max_winning_per_player;
+            contributions_[pid] -= winnings_from_pid;
+            total += winnings_from_pid;
+        }
+    }
+
+    return total;
 }
 
 }
