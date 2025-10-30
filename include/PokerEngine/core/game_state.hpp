e054542cb13e1f4bee5bc231043d13791f3baceb
Stephen Liu <138885345+shl211@users.noreply.github.com>
2025-10-30 18:48:24 +0000

    Feature/game state (#24)
    * Add simple abstractions for game state
    
    * Add game state abstraction and refactor existing simulators

---
+++ include/PokerEngine/core/game_state.hpp
@@ -0,0 +1,22 @@
+#ifndef POKER_ENGINE_CORE_GAME_STATE_HPP
+#define POKER_ENGINE_CORE_GAME_STATE_HPP
+
+#include <vector>
+
+#include "PokerEngine/core/board.hpp"
+#include "PokerEngine/core/pot.hpp"
+#include "PokerEngine/core/deck.hpp"
+#include "PokerEngine/core/player_state.hpp"
+
+namespace PokerEngine::Core {
+
+struct GameState {
+    std::vector<PlayerState> players;
+    Board board;
+    Pot pot;
+    Deck deck;
+};
+
+}
+
+#endif
---
+++ include/PokerEngine/core/player_state.hpp
@@ -0,0 +1,20 @@
+#ifndef POKER_ENGINE_CORE_PLAYER_STATE_HPP
+#define POKER_ENGINE_CORE_PLAYER_STATE_HPP
+
+#include "PokerEngine/core/hand.hpp"
+#include "PokerEngine/core/range.hpp"
+#include "PokerEngine/core/stack.hpp"
+
+namespace PokerEngine::Core {
+
+struct PlayerState {
+    int id;
+    Stack stack;
+    Hand holeCards;
+    bool hasFolded { false };
+    bool isAllIn { false };
+    Range range;
+};
+}
+
+#endif
